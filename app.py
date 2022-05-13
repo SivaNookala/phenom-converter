1382d717861e2356e134ba763a80258bfc75443d
Siva Kumar Nookala <nookala.sivakumar@gmail.com>
2022-05-13 22:04:50 +0530

    Initial checkin

---
+++ app.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import eng_to_ipa as p
+
+app = Flask(__name__)
+
+
+@app.route('/engToIpa/<text>', methods=['GET'])
+def translateEngToIpa(text):
+    return p.convert(text)
+
+
+if __name__ == '__main__':
+   app.run()
