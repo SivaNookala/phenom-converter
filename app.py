9c025da5e0219ea3600e220ae7418078f2b86f41
Siva Kumar Nookala <nookala.sivakumar@gmail.com>
2022-05-13 22:18:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ import eng_to_ipa as p
 
 app = Flask(__name__)
 
-
 @app.route('/engToIpa/<text>', methods=['GET'])
 def translateEngToIpa(text):
     return p.convert(text)
-
 
 if __name__ == '__main__':
    app.run()
